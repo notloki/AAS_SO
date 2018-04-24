7e2d75bae1b92480a80dff1192a1c164f84d3fe2
brianburke1030@live.com <brianburke1030@live.com>
2018-04-24 03:22:02 -0500

    stuff

--- src/main/java/com/notloki/aas_so/Controller.java
+++ src/main/java/com/notloki/aas_so/Controller.java
@@ -1,13 +1,14 @@
 package com.notloki.aas_so;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Controller {
 
-    public Label logo;
+    public Hyperlink logo;
 
 
 
@@ -19,6 +20,12 @@ public class Controller {
         imageView.setFitWidth(160);
         imageView.setFitHeight(130);
         logo.setGraphic(imageView);
+
+
+    }
+    @FXML
+    private void email() {
+
     }
 
 
