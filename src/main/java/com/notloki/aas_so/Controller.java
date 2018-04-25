e5fc44245463bbfbf17754d312055205938a5868
brianburke1030@live.com <brianburke1030@live.com>
2018-04-25 09:12:02 -0500

    more stuff

--- src/main/java/com/notloki/aas_so/Controller.java
+++ src/main/java/com/notloki/aas_so/Controller.java
@@ -1,6 +1,8 @@
 package com.notloki.aas_so;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -8,25 +10,24 @@ import javafx.scene.image.ImageView;
 
 public class Controller {
 
-    public Hyperlink logo;
+    @FXML
+    private ComboBox shipTo;
+
 
 
 
     @FXML
     private void initialize() {
+        //        Image image = new Image(getClass().getResourceAsStream("/AASLogo.jpg"));
+//        ImageView imageView = new ImageView(image);
+//        imageView.setFitWidth(160);
+//        imageView.setFitHeight(130);
+//        logo.setGraphic(imageView);
 
-        Image image = new Image(getClass().getResourceAsStream("/AASLogo.jpg"));
-        ImageView imageView = new ImageView(image);
-        imageView.setFitWidth(160);
-        imageView.setFitHeight(130);
-        logo.setGraphic(imageView);
-
-
-    }
-    @FXML
-    private void email() {
 
     }
+
+
 
 
 
