4a72321c5f15c9773e25feb12cd487455ddd34bc
brianburke1030@live.com <brianburke1030@live.com>
2018-04-28 03:37:22 -0500

    initial

--- src/main/java/com/notloki/aas_so/Main.java
+++ src/main/java/com/notloki/aas_so/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/rootLayout.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/rootLogin.fxml"));
         primaryStage.setTitle(Ref.COMPANY + " " + Ref.PROGRAM);
         primaryStage.getIcons().add(new Image("/AASLogo.jpg"));
         primaryStage.setScene(new Scene(root, 900, 700));
